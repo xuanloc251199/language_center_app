e8d8072873de4fb7e971223046dab3e485341655
xuanloc2511 <maixuanloc0101@gmail.com>
2023-09-27 22:26:45 +0700

    update home activity

---
+++ app/src/main/java/com/vangiau/language_center_app/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package com.vangiau.language_center_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
